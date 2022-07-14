3488c532a98f4fa021a006f7bead1d4290d2e182
chanpm26 <little26pc@yahoo.com>
2022-07-14 18:31:00 -0400

    Add functional delete button

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let myLibrary = []
 const watershipDown = new Book('Watership Down', 'Richard Adams', 413, "Read")
 addBook(watershipDown)
 
-const thePlague = new Book('The Plague', 'Albert Camus', 250, "Read")
+const thePlague = new Book('The Plague', 'Albert Camus', 278, "Read")
 addBook(thePlague)
 
-const maximumRide = new Book('Maximum Ride', 'James Patterson', 350, "Read")
+const maximumRide = new Book('Maximum Ride', 'James Patterson', 350, "Not Read")
 addBook(maximumRide)
 
 function Book(title, author, pages, read) {
@@ -16,7 +16,7 @@ function Book(title, author, pages, read) {
     this.read = read,
     this.info = function () {
         return (`${title} by ${author} is 
-        ${pages}pages.
+        ${pages} pages.
         ${read}`) 
     }
 }
@@ -30,9 +30,24 @@ const display = document.getElementById("library")
 function displayBook(book) {
     for (let i = 0; i < book.length; i++) {
     let oneBook = document.createElement("div");
-    oneBook.textContent = book[i].info();
-    display.appendChild(oneBook)
+    oneBook.innerText = book[i].info();
+    oneBook.classList.add("book");
+    oneBook.setAttribute('id', `${i}`);
+    display.appendChild(oneBook);
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("deleteButton");
+    deleteButton.setAttribute('data-id', `${i}`)
+    deleteButton.textContent = "Delete";
+    oneBook.appendChild(deleteButton);
+    // let readButton = document.createElement("button");
+    // readButton.classList.add("readButton");
+    // oneBook.appendChild(readButton);
+    if (book[i].read == "Read") {
+        oneBook.classList.add("read")
+    } else {
+        oneBook.classList.add("notRead")
     }
+}
 }
 
 displayBook(myLibrary)
@@ -54,3 +69,28 @@ const submit = document.getElementById('submit')
 submit.addEventListener('click', function () {
     document.getElementById('form-container').classList.remove("show")
 })
+
+let deleteButtons = Array.from(document.querySelectorAll('.deleteButton'))
+
+
+let deleteTask = deleteButtons.forEach(function(button) {
+    button.addEventListener('click', function () {
+        let index = button.dataset.id;
+        console.log(index);
+        myLibrary.splice(index, 1);
+        let deletedBook = document.getElementById(index);
+        display.removeChild(deletedBook);
+        deleteButtons.splice(index, 1);
+    })
+})
+
+// const bookRead = document.getElementsByClassName('readButton')
+// bookRead.addEventListener('click', function () {
+//     if (div.classList.contains("notRead")) {
+//         div.classList.remove("notRead");
+//         div.classList.add("read")
+//     } else if (div.classList.contains("read")) {
+//         div.classList.remove("read");
+//         div.classList.add("notRead")
+//     }
+// }) 
